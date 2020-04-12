aa46df02b5d605d1db6e81753a4fb9157bfb2815
Sonu Malik <iamsonumalik@gmail.com>
2020-04-13 00:15:35 +0530

    Updates

---
+++ precache-manifest.01e65e3c922ba9037fece3ba4f45c54a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c465a0163088e800d8178cb15eae6ec2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1a7f4fc675d77703cf3",
+    "url": "/static/css/main.7747be13.chunk.css"
+  },
+  {
+    "revision": "7d469a3c2b64056bc629",
+    "url": "/static/js/2.bd5efa54.chunk.js"
+  },
+  {
+    "revision": "9416fd4c4cf7ae2ee54a759dffdcd421",
+    "url": "/static/js/2.bd5efa54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1a7f4fc675d77703cf3",
+    "url": "/static/js/main.681c1fe9.chunk.js"
+  },
+  {
+    "revision": "0550c4086835eb075d92",
+    "url": "/static/js/runtime-main.06c8c3ed.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "412b7a47db4b1f76eef3d7f22f9889a7",
+    "url": "/static/media/hire.412b7a47.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "4b1aa14f04fb4768ddf6d83698c90d2b",
+    "url": "/static/media/workwithus.4b1aa14f.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0c0cdcfbf78fde9cc5612b1bef8a92e1.js"
+  "/precache-manifest.01e65e3c922ba9037fece3ba4f45c54a.js"
 );
 
 self.addEventListener('message', (event) => {
